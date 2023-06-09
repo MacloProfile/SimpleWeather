4c122d2dff480dbff975cece007eb129c7a84d64
Denis K <deniskommcl@gmail.com>
2023-06-09 11:16:09 +0300

    release v1

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        CreatePageOfWeather createPageOfWeather = new CreatePageOfWeather();
+        createPageOfWeather.printWeather();
+    }
+}
